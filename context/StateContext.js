7f2c4ab3441456e9f224b0ddd90b84d384d47f0f
pranoy <chandra.pranay@yahoo.in>
2022-10-08 22:02:23 +0530

    added logic for update product from cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,10 +6,13 @@ const Context = createContext()
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false)
   const [cartItems, setCartItems] = useState([])
-  const [totalPrice, setTotalPrice] = useState()
+  const [totalPrice, setTotalPrice] = useState(0)
   const [totalQuantities, setTotalQuantities] = useState(0)
   const [qty, setQty] = useState(1)
 
+  // variables to update items in cart.
+  let foundProduct;
+  let index
 
   const incQty = () => {
     setQty((prev) => prev + 1)
@@ -26,19 +29,19 @@ export const StateContext = ({ children }) => {
 
   const onAdd = (product, quantity) => {
 
-     // check if the product is already in the cart.
+    // check if the product is already in the cart.
     const checkProductInCart = cartItems.find((item) => item._id === product._id)
-    
+
     setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
-    
+
     if (checkProductInCart) {
 
       const updatedCartItems = cartItems.map((cartProduct) => {
 
         // if we try to add to cart the same product that is already present in the cart, 
         //then it should inc the quantity on the consisting item, not add another new item.
-        if(cartProduct._id === product._id) return {
+        if (cartProduct._id === product._id) return {
           ...cartProduct,
           quantity: cartProduct.quantity + quantity
         }
@@ -48,11 +51,35 @@ export const StateContext = ({ children }) => {
     // if the item does not already existed in the cart.
     else {
       product.quantity = quantity
-      setCartItems([...cartItems, {...product}])
+      setCartItems([...cartItems, { ...product }])
     }
-    
+
     toast.success(`${qty} ${product.name} added to the cart.`)
-    
+
+  }
+
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id)
+
+    index = cartItems.findIndex((product) => product._id === id);
+
+    const newCartItems = cartItems.filter((item) => item._id !== id)
+
+    if(value === 'inc') {
+
+      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+
+    } else if(value === 'dec') {
+
+      if (foundProduct.quantity > 1) {
+        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+
+      }
+    }
   }
 
   return (
@@ -66,7 +93,8 @@ export const StateContext = ({ children }) => {
         incQty,
         decQty,
         onAdd,
-        setShowCart
+        setShowCart,
+        toggleCartItemQuantity
       }}
     >
       {children}
