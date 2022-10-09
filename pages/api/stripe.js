24a591928a8774b706404a1f6ba61c26c1cdaf51
pranoy <chandra.pranay@yahoo.in>
2022-10-09 11:40:42 +0530

    added stripe gateway successfully

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,9 +1,14 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 
 export default async function handler(req, res) {
+
+  
   if (req.method === 'POST') {
+
+    console.log(`CartItems ==> ${req.body}`);
+
     try {
       const params = {
         submit_type: 'pay',
@@ -11,15 +16,17 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1LqrDLSGEIuJgqq8O1iXZ9a9' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/3td86e7p/production/').replace('-webp', '.webp');
+
+          console.log(`New Image ==> ${newImage}`);
 
           return {
             price_data: { 
-              currency: 'usd',
+              currency: 'inr',
               product_data: { 
                 name: item.name,
                 images: [newImage],
