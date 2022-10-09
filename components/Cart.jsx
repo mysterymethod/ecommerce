0bf1f614365db0d4c55c2baccd3c7d84635f46e3
pranoy <chandra.pranay@yahoo.in>
2022-10-09 10:09:38 +0530

    integrating stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -14,6 +14,12 @@ const Cart = () => {
 
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext()
 
+  const handleCheckout = () => {
+
+  }
+
+  
+
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className="cart-container">
@@ -75,7 +81,7 @@ const Cart = () => {
               <h3>₹{totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type='button' className='btn' onClick=''>
+              <button type='button' className='btn' onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,5 @@
+// Backend for out application, not going to render on the frontend. In nextjs we dont have node or express server.
+//
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
